4da099b162cae4f517d36f659aaa45e92cb3de7b
Alex Sabetti <59347621+AlexSabetti@users.noreply.github.com>
2025-02-13 20:26:09 -0500

    Update ClickableObject.gd

--- Scripts/ClickableObject.gd
+++ Scripts/ClickableObject.gd
@@ -2,6 +2,7 @@ class_name ClickableObject
 extends Node3D
 @export_group("Object Specifics")
 @export var object_name: String
+@export var obj_id: int
 
 # This is for when the player clicks on the item but isn't in the proper section (This would happen usually by accident). It just denies the signal unless it's true.
 @export var can_interact: bool = false
@@ -15,4 +16,6 @@ func _input_event(viewport: Viewport, event: InputEvent, shape_idx: int):
 	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
 		if can_interact:
 			signal_manager.emit_signal("object_clicked", object_name)
+
+func _activate_in_scene(
 		
