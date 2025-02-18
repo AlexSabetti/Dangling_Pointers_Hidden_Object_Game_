0f894fd22e234a8ff389ee4f69113a0364e39670
Ganglystem <colinparkin1@yahoo.com>
2025-02-17 20:55:41 -0500

    more prop and texture work
    tweaked some props, added some textures, and tweaked lighting

--- Scripts/boat_1.gd
+++ Scripts/boat_1.gd
@@ -2,10 +2,12 @@ extends Node3D
 
 class_name BoatController
 
+@export var isMenuBoat := false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	if isMenuBoat:
+		$cameraController.controls_disabled = true
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
