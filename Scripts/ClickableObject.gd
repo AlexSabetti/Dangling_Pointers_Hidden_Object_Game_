1122cf2903bf50343bc6c4db5698ac02f0471b56
AlexSabetti <Soyfalsoescapeplan@gmail.com>
2025-02-19 01:02:43 -0500

    work

--- Scripts/ClickableObject.gd
+++ Scripts/ClickableObject.gd
@@ -3,6 +3,7 @@ extends Node3D
 @export_group("Object Specifics")
 @export var object_name: String
 @export var obj_id: int
+@onready var col_box: CollisionShape3D = $CollisionShape3D
  # For item interaction requirements, like not being interactable until other objects have been picked up
 @export var requirements: Array = []
 
@@ -13,12 +14,7 @@ var signal_manager: SignalBus = Bus
 
 func _ready():
 	set_process_input(true)
-	signal_manager.connect("camera_changed", _activate_in_scene) 
-
-func _input_event(event: InputEvent):
-	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
-		if can_interact and requirements.size() == 0:
-			signal_manager.emit_signal("object_clicked", self)
+	signal_manager.connect("camera_changed", _activate_in_scene)
 
 func _activate_in_scene(id_to_activate: int):
 	if id_to_activate == obj_id:
